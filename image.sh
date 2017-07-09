5d37340715d6daa1621642c07de4f4847428ced1
michaelenglert <michi.eng@gmail.com>
2017-07-09 21:29:26 +0200

    image name will be read and used

--- image.sh
+++ image.sh
@@ -9,9 +9,12 @@ read user
 echo -e "\x1B[7mPortal Password\x1B[27m"
 read -s password
 
+echo -e "\x1B[7mImage Name\x1B[27m"
+read image
+
 docker build \
 --build-arg USER=$user \
 --build-arg PASSWORD=$password \
 --build-arg BASEURL=https://aperture.appdynamics.com/download/prox/download-file \
 --build-arg VERSION=$version \
--t michaelenglert/appd-agents:$version .
+-t $image:$version .
