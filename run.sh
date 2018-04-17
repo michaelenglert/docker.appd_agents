5a8f0306907c0f4dc098ef0d5a04ad5f1b8506e4
Michi <michi.eng@gmail.com>
2018-04-17 17:02:27 +0200

    changed repo structure. changed run script

--- run.sh
+++ run.sh
@@ -1,12 +1,40 @@
+#!/bin/bash
+
+echo -e "\x1B[7mImage Name\x1B[27m"
+read image
+
+echo -e "\x1B[7mController Host\x1B[27m"
+read controller_host
+
+echo -e "\x1B[7mController Port\x1B[27m"
+read controller_port
+
+echo -e "\x1B[7mSSL enabled\x1B[27m"
+read ssl_enabled
+
+echo -e "\x1B[7mAccount Name\x1B[27m"
+read account_name
+
+echo -e "\x1B[7mAccess Key\x1B[27m"
+read access_key
+
+echo -e "\x1B[7mSIM enabled\x1B[27m"
+read sim_enabled
+
+echo -e "\x1B[7mDocker enabled\x1B[27m"
+read docker_enabled
+
 docker run -d --name appd-agents \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /:/hostroot:ro \
     -v /tmp/app-agent:/app-agent \
-    -e APPDYNAMICS_CONTROLLER_HOST_NAME="" \
-    -e APPDYNAMICS_CONTROLLER_PORT="" \
-    -e APPDYNAMICS_AGENT_ACCOUNT_NAME="customer1" \
-    -e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY="" \
-    -e APPDYNAMICS_SIM_ENABLED="true" \
-    -e APPDYNAMICS_CONTROLLER_SSL_ENABLED="false" \
-    -e APPDYNAMICS_DOCKER_ENABLED="true" \
-    michi/appd-agents:4.3.7.3
+    -e APPDYNAMICS_CONTROLLER_HOST_NAME="$controller_host" \
+    -e APPDYNAMICS_CONTROLLER_PORT="$controller_port" \
+    -e APPDYNAMICS_AGENT_ACCOUNT_NAME="$account_name" \
+    -e APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY="$access_key" \
+    -e APPDYNAMICS_SIM_ENABLED="$sim_enabled" \
+    -e APPDYNAMICS_CONTROLLER_SSL_ENABLED="$ssl_enabled" \
+    -e APPDYNAMICS_DOCKER_ENABLED="$docker_enabled" \
+    $image:latest
+
+exit 0
