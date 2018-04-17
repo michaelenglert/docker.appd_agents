1bbdbd1cf255c6f5d4403d2aefd447f1c353856f
Michi <michi.eng@gmail.com>
2018-04-17 23:29:45 +0200

    Fixed controller-info.xml bug

--- docker/appd.sh
+++ docker/appd.sh
@@ -2,8 +2,8 @@
 
 # Configure App Agent
 find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-host>/c\<controller-host>$APPDYNAMICS_CONTROLLER_HOST_NAME<\/controller-host>\" {}" \;
-find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-port>/c\<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-host>\" {}" \;
-find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-ssl-enabled>/c\<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL_ENABLED<\/controller-host>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-port>/c\<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-ssl-enabled>/c\<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL_ENABLED<\/controller-ssl-enabled>\" {}" \;
 find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<account-name>/c\<account-name>$APPDYNAMICS_AGENT_ACCOUNT_NAME<\/account-name>\" {}" \;
 find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<account-access-key>/c\<account-access-key>$APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY<\/account-access-key>\" {}" \;
 find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<agent-runtime-dir>/c\<agent-runtime-dir>\/<\/agent-runtime-dir>\" {}" \;
