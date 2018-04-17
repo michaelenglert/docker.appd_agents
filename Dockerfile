70fdbaa32c144c0a73ed3b28b83b9850ea7eb32f
Michi <michi.eng@gmail.com>
2018-04-17 16:54:26 +0200

    changed paths

--- Dockerfile
+++ Dockerfile
@@ -6,28 +6,29 @@ ARG MACHINE_AGENT_VERSION
 ARG APP_AGENT_VERSION
 
 ENV APPDYNAMICS_CONTROLLER_HOST_NAME="" \
-    APPDYNAMICS_CONTROLLER_PORT="" \
-    APPDYNAMICS_AGENT_ACCOUNT_NAME="" \
+    APPDYNAMICS_CONTROLLER_PORT="8090" \
+    APPDYNAMICS_AGENT_ACCOUNT_NAME="customer1" \
     APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY="" \
-    APPDYNAMICS_SIM_ENABLED="" \
-    APPDYNAMICS_DOCKER_ENABLED="" \
-    APPDYNAMICS_CONTROLLER_SSL_ENABLED=""
+    APPDYNAMICS_SIM_ENABLED="false" \
+    APPDYNAMICS_DOCKER_ENABLED="false" \
+    APPDYNAMICS_CONTROLLER_SSL_ENABLED="false"
 
-VOLUME /app-agent
+VOLUME /opt/appdynamics/appagent
 
-ADD setup_agent.sh /
+ADD setup_agent.sh /opt/appdynamics/
 
 RUN apt-get update \
     && apt-get install -q -y --fix-missing unzip curl \
-    && chmod +x /setup_agent.sh \
-    && mkdir /machine-agent \
-    && curl -L -o /machine-agent.zip $BASEURL/machine/$MACHINE_AGENT_VERSION/machineagent-bundle-64bit-linux-$MACHINE_AGENT_VERSION.zip \
-    && curl -L -o /app-agent.zip $BASEURL/java/$APP_AGENT_VERSION/AppServerAgent-$APP_AGENT_VERSION.zip \
-    && unzip /machine-agent.zip -d /machine-agent \
-    && unzip /app-agent.zip -d /app-agent-temp/ \
+    && chmod +x /opt/appdynamics/setup_agent.sh \
+    && mkdir -p /opt/appdynamics/machineagent \
+    && mkdir -p /opt/appdynamics/appagent \
+    && curl -L -o /tmp/machineagent.zip $BASEURL/machine/$MACHINE_AGENT_VERSION/machineagent-bundle-64bit-linux-$MACHINE_AGENT_VERSION.zip \
+    && curl -L -o /tmp/appagent.zip $BASEURL/java/$APP_AGENT_VERSION/AppServerAgent-$APP_AGENT_VERSION.zip \
+    && unzip /tmp/machineagent.zip -d /opt/appdynamics/machineagent \
+    && unzip /tmp/appagent.zip -d /opt/appdynamics/appagenttemp \
     && apt-get remove --purge -q -y curl unzip \
     && apt-get autoremove -q -y \
     && apt-get clean -q -y \
-    && rm -rf /machine-agent.zip /app-agent.zip /cookies.txt /tmp/*
+    && rm -rf /tmp/*
 
-CMD /bin/bash -c "/setup_agent.sh" && /machine-agent/bin/machine-agent start
+CMD /bin/bash -c "/opt/appdynamics/setup_agent.sh" && /opt/appdynamics/machineagent/bin/machine-agent start
--- setup_agent.sh
+++ setup_agent.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-sed -i -e "/<controller-host>/c\<controller-host>$APPDYNAMICS_CONTROLLER_HOST_NAME<\/controller-host>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<controller-port>/c\<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<controller-ssl-enabled>/c\<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL_ENABLED<\/controller-ssl-enabled>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<account-name>/c\<account-name>$APPDYNAMICS_AGENT_ACCOUNT_NAME<\/account-name>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<account-access-key>/c\<account-access-key>$APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY<\/account-access-key>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<agent-runtime-dir>/c\<agent-runtime-dir>\/<\/agent-runtime-dir>" /app-agent-temp/conf/controller-info.xml
-sed -i -e "/<\!-- property name=\"config-poll-interval\"/c\<property name=\"appdynamics.jvm.shutdown.mark.node.as.historical\" value=\"true\" \/>" /app-agent-temp/ver*/conf/app-agent-config.xml
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-host>/c\<controller-host>$APPDYNAMICS_CONTROLLER_HOST_NAME<\/controller-host>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-port>/c\<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-host>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<controller-ssl-enabled>/c\<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL_ENABLED<\/controller-host>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<account-name>/c\<account-name>$APPDYNAMICS_AGENT_ACCOUNT_NAME<\/account-name>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<account-access-key>/c\<account-access-key>$APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY<\/account-access-key>\" {}" \;
+find /opt/appdynamics/appagenttemp/ -iname controller-info.xml -exec /bin/bash -c "sed -i -e \"/<agent-runtime-dir>/c\<agent-runtime-dir>\/<\/agent-runtime-dir>\" {}" \;
 
-cp /app-agent-temp/conf/controller-info.xml /app-agent-temp/ver*/conf/controller-info.xml
-
-cp -r /app-agent-temp/* /app-agent/
-rm -rf /app-agent-temp
+cp -r /opt/appdynamics/appagenttemp/* /opt/appdynamics/appagent
+rm -rf /opt/appdynamics/appagenttemp/
 
 exit 0
